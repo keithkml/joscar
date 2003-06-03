9f5cb4ee36ef2871209cd829a82d0e25d3e78aaf
kano <kano@520820fe-6d0d-0410-88f7-97391484ac63>
2003-06-03 17:47:07 +0000

    refactoring . . .

--- trunk/joscar/src/net/kano/joscar/ratelim/ConnectionQueueMgr.java
+++ trunk/joscar/src/net/kano/joscar/ratelim/ConnectionQueueMgr.java
@@ -2,31 +2,31 @@
  *  Copyright (c) 2003, The Joust Project
  *  All rights reserved.
  *
- *  Redistribution and use in source and binary forms, with or without 
- *  modification, are permitted provided that the following conditions 
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
  *  are met:
  *
- *  - Redistributions of source code must retain the above copyright 
- *    notice, this list of conditions and the following disclaimer. 
- *  - Redistributions in binary form must reproduce the above copyright 
- *    notice, this list of conditions and the following disclaimer in 
- *    the documentation and/or other materials provided with the 
- *    distribution. 
- *  - Neither the name of the Joust Project nor the names of its 
- *    contributors may be used to endorse or promote products derived 
- *    from this software without specific prior written permission. 
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of the Joust Project nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
  *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  *
  *  File created by keith @ May 25, 2003
@@ -45,7 +45,7 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Iterator;
 
-public class RateClassSet {
+class ConnectionQueueMgr {
     private final RateLimitingQueueMgr queueMgr;
     private final SnacProcessor snacProcessor;
     private Map classToQueue = new HashMap();
@@ -53,7 +53,7 @@ public class RateClassSet {
     private RateQueue defaultQueue = null;
     private boolean paused = false;
 
-    public RateClassSet(RateLimitingQueueMgr queueMgr,
+    public ConnectionQueueMgr(RateLimitingQueueMgr queueMgr,
             SnacProcessor snacProcessor) {
         DefensiveTools.checkNull(queueMgr, "queueMgr");
         DefensiveTools.checkNull(snacProcessor, "snacProcessor");
@@ -69,8 +69,6 @@ public class RateClassSet {
     public void queueSnac(SnacRequest request) {
         DefensiveTools.checkNull(request, "request");
 
-        System.out.println("queueing within rate class set..");
-
         CmdType type = CmdType.ofCmd(request.getCommand());
 
         RateQueue queue;
@@ -82,13 +80,12 @@ public class RateClassSet {
 
         if (queue == null) {
             // so there's no queue. let's send it right out!
-            System.out.println("sending immediately..");
             queueMgr.sendSnac(snacProcessor, request);
             return;
         }
 
         queue.enqueue(request);
-        queueMgr.getRunner().update();
+        queueMgr.getRunner().update(queue);
     }
 
     public synchronized void setRateClass(RateClassInfo rateInfo) {
@@ -97,7 +94,6 @@ public class RateClassSet {
         RateQueue queue = updateRateQueue(rateInfo);
 
         CmdType[] cmdTypes = rateInfo.getCommands();
-        System.out.println("cmdTypes: " + cmdTypes);
         if (cmdTypes != null) {
             if (cmdTypes.length == 0) {
                 // if there aren't any member SNAC commands for this rate
@@ -114,7 +110,7 @@ public class RateClassSet {
         }
 
         // something most likely changed.
-        queueMgr.getRunner().update();
+        queueMgr.getRunner().update(queue);
     }
 
     public void updateRateClass(int changeCode, RateClassInfo rateInfo) {
