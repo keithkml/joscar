dd0a739f0e0d3875864c6b4461d709719b8aa9a8
kano <kano@520820fe-6d0d-0410-88f7-97391484ac63>
2004-02-25 07:36:10 +0000

    fancy

---
+++ oscarjar/trunk/joustsim/src/net/kano/aimcrypto/temps/EndAvoider.java
@@ -0,0 +1,67 @@
+/*
+ *  Copyright (c) 2004, The Joust Project
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without 
+ *  modification, are permitted provided that the following conditions 
+ *  are met:
+ *
+ *  - Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer. 
+ *  - Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in 
+ *    the documentation and/or other materials provided with the 
+ *    distribution. 
+ *  - Neither the name of the Joust Project nor the names of its 
+ *    contributors may be used to endorse or promote products derived 
+ *    from this software without specific prior written permission. 
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  File created by keith @ Feb 24, 2004
+ *
+ */
+
+package net.kano.aimcrypto.temps;
+
+import net.kano.joscar.DefensiveTools;
+
+import javax.swing.text.NavigationFilter;
+import javax.swing.text.Document;
+import javax.swing.text.Position;
+
+public class EndAvoider extends NavigationFilter {
+    private final Document doc;
+
+    public EndAvoider(Document doc) {
+        DefensiveTools.checkNull(doc, "doc");
+
+        this.doc = doc;
+    }
+
+    public void moveDot(NavigationFilter.FilterBypass fb, int dot,
+            Position.Bias bias) {
+        super.moveDot(fb, fixDot(dot), bias);
+    }
+
+    public void setDot(NavigationFilter.FilterBypass fb, int dot,
+            Position.Bias bias) {
+        super.setDot(fb, fixDot(dot), bias);
+    }
+
+    private int fixDot(int dot) {
+        if (dot == doc.getLength() && dot > 0) return dot - 1;
+        else return dot;
+    }
+}
