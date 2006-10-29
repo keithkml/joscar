119d4065f01e1fd3a68a3ae7bb201e4360df1a40
tekjew <tekjew@520820fe-6d0d-0410-88f7-97391484ac63>
2006-10-29 17:46:15 +0000

    When a ClientFlapConn disconnects or fails, AsynchronousFlapProcessor.breakdown() is now called on its AsynchronousFlapProcessor, and flapProcessor instance variable is cleared.
    AsynchronousFlapProcessor.breakdown() causes the weak reference its thread uses to determine when to die to be immediately cleared.  It also detaches its thread and clears its observers lists.
    
    This is necessary because AsynchronousFlapProcessor creates a thread which continues until the AsynchronousFlapProcessor itself is GC'd.  As I understand it based on YourKit Java Profiler analysis, that never happened previously,  because the observers list for the AsynchronousFlapProcessor includes an OscarConnection which keeps track of the ClientFlapConn which keeps track of the AsynchronousFlapProcessor... a loop starting with a GC root.
    
    The ClientFlapConn now breaks that vicious cycle manually, allowing the thread to die.
    
    This does not solve every leak, but it appears to fix the biggest one we've observed in Adium 1.0b14.

--- trunk/protocol/src/net/kano/joscar/flap/AsynchronousFlapProcessor.java
+++ trunk/protocol/src/net/kano/joscar/flap/AsynchronousFlapProcessor.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.net.Socket;
 import java.util.LinkedList;
+import net.kano.joscar.logging.Logger;
+import net.kano.joscar.logging.LoggingSystem;
 
 /**
  * Created by IntelliJ IDEA. User: klea Date: Aug 8, 2006 Time: 6:38:12 PM To
@@ -15,9 +17,14 @@ public class AsynchronousFlapProcessor extends AbstractFlapProcessor {
 
     private final Thread thread;
     private static final FlapPacket[] EMPTY_FLAP_ARRAY = new FlapPacket[0];
+	
+	private final WeakReference<AsynchronousFlapProcessor> processorReference = new WeakReference<AsynchronousFlapProcessor>(this);
+
+	private static final Logger LOGGER
+		= LoggingSystem.getLogger(AsynchronousFlapProcessor.class.getName());
 
     {
-        thread = new Thread(new BackgroundPacketProcessor(this),
+        thread = new Thread(new BackgroundPacketProcessor(processorReference),
                 "FLAP processor");
         thread.start();
     }
@@ -26,8 +33,16 @@ public class AsynchronousFlapProcessor extends AbstractFlapProcessor {
     }
 
     public AsynchronousFlapProcessor(Socket socket) throws IOException {
-        super(socket);
+        super(socket);		
     }
+	
+	protected void finalize() throws Throwable {
+		try {
+			LOGGER.logFine(this + ": ** finalize() **");
+		} finally {
+			super.finalize();
+		}
+	}
 
     protected void handlePacket(FlapPacket packet) {
         synchronized(lock) {
@@ -37,22 +52,21 @@ public class AsynchronousFlapProcessor extends AbstractFlapProcessor {
     }
 
     private static class BackgroundPacketProcessor implements Runnable {
-        private final WeakReference<AsynchronousFlapProcessor>
-                processorReference;
-
-        public BackgroundPacketProcessor(AsynchronousFlapProcessor processor) {
-            processorReference
-                    = new WeakReference<AsynchronousFlapProcessor>(processor);
+		private final WeakReference<AsynchronousFlapProcessor> backgroundProcessorReference;
+		
+        public BackgroundPacketProcessor(WeakReference<AsynchronousFlapProcessor> inProcessorReference) {
+			backgroundProcessorReference = inProcessorReference;
         }
 
         public void run() {
             while (true) {
-                if (!tryProcessingPackets()) break;
+                if (!tryProcessingPackets()) 
+					break;
             }
         }
 
         private boolean tryProcessingPackets() {
-            AsynchronousFlapProcessor processor = processorReference.get();
+            AsynchronousFlapProcessor processor = backgroundProcessorReference.get();
             if (processor == null) {
                 return false;
             }
@@ -88,4 +102,12 @@ public class AsynchronousFlapProcessor extends AbstractFlapProcessor {
         list.clear();
         return packets;
     }
+	
+	public void breakdown() {
+		synchronized(lock) {
+			processorReference.clear();
+
+			super.breakdown();
+		}
+	}
 }
